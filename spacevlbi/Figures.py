Fixed bug in elevation calculation in Orbit.py. Inconsistency in ground station and spacecraft position units. Fixed units in figure labels.

spacevlbi/Figures.py
@@ -674,8 +674,8 @@ def GroundStationElevation(spaceTelescopes, groundStations, simTime, \
                 ax.plot(time, elevation, label=name)
                 
             # Configure axes
-            ylabel = spaceTelescopes[telescopeSelect].name + r" Elevation Angle [$ \
-                \degree $]"
+            unit = r" Elevation Angle [$ \degree $]"
+            ylabel = spaceTelescopes[telescopeSelect].name + unit
             ax.set(ylim=(0, 90))
             ax.set_xlabel('Time')
             ax.set_ylabel(ylabel)
@@ -866,9 +866,9 @@ def ECEF_to_LLA(ecef):
     :rtype: np.array
     """
 	# x, y and z are scalars or vectors in meters
-    x = ecef[0]*1000
-    y = ecef[1]*1000
-    z = ecef[2]*1000
+    x = ecef[0]
+    y = ecef[1]
+    z = ecef[2]
 
     a=6378137
     e_sq = 6.69437999014e-3

spacevlbi/Orbit.py
@@ -258,7 +258,7 @@ def SatGroundAccess(spaceTelescopes, groundStations, time):
                                     'cartesian', obstime = time)
                     eci = (ecef.transform_to(GCRS)).cartesian    
                     eciPosition = np.array([eci.x.value, eci.y.value, \
-                                    eci.z.value]).reshape((3,1)) << u.m
+                                    eci.z.value]).reshape((3,1))*1000 << u.m
                     # Update ground station position
                     groundStations[k].eciPosition = np.vstack((groundStations[k].eciPosition, \
                                     eciPosition.reshape((1,3))))